b6b7cb2e521715c98e7f29fc0d113d79cb037019
victor <vgm@gmail.com>
2025-09-18 08:59:06 +0200

    Fonction commenté

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
+/**
+ * Change la couleur du message donné selon le nom de la couleur spécifié.
+ * Cette fonction utilise les codes d'échappement ANSI pour appliquer une couleur à la sortie console. Si un nom de couleur
+ * non reconnu ou une chaîne vide est fourni, aucune couleur n'est appliquée.
+ *
+ * @param message Le message auquel la couleur sera appliquée.
+ * @param couleur Le nom de la couleur à appliquer (ex: "rouge", "vert", "bleu"). Par défaut c'est une chaîne vide, ce qui n'applique aucune couleur.
+ * @return Le message coloré sous forme de chaîne, ou le même message si aucune couleur n'est appliquée.
+ */
 fun changeCouleur(message: String, couleur:String=""): String {
     val reset = "\u001B[0m"
     val codeCouleur = when (couleur.lowercase()) {
@@ -15,9 +24,10 @@ fun changeCouleur(message: String, couleur:String=""): String {
     }
     return "$codeCouleur$message$reset"
 }
+
 fun main() {
     println(changeCouleur("Hello","rouge"))
     println(changeCouleur("World","bleu"))
     println("Hello ${changeCouleur("my","jaune")} World")
-    println(changeCouleur("Truc","marron"))
+    println(changeCouleur("truc","marron"))
 }
