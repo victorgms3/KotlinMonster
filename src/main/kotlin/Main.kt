abdbb08d591bd8079c104ed711509491eada2ccb
victor <vgm@gmail.com>
2025-09-18 08:49:16 +0200

    Test de couleur

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun changeCouleur(message: String, couleur:String=""): String {
+    val reset = "\u001B[0m"
+    val codeCouleur = when (couleur.lowercase()) {
+        "rouge" -> "\u001B[31m"
+        "vert" -> "\u001B[32m"
+        "jaune" -> "\u001B[33m"
+        "bleu" -> "\u001B[34m"
+        "magenta" -> "\u001B[35m"
+        "cyan" -> "\u001B[36m"
+        "blanc" -> "\u001B[37m"
+        else -> "" // pas de couleur si non reconnu
     }
+    return "$codeCouleur$message$reset"
+}
+fun main() {
+    println(changeCouleur("Hello","rouge"))
+    println(changeCouleur("World","bleu"))
+    println("Hello ${changeCouleur("my","jaune")} World")
+    println(changeCouleur("Truc","marron"))
 }
